d7390d94c1bcac135e4cae9716d68f092bbae60a
Felipe Santana <felipe012santana012@gmail.com>
2023-07-31 07:25:19 -0300

    ajustes na tela de form endereço

--- principal/js/servico_js/form_solicitacao_servico.js
+++ principal/js/servico_js/form_solicitacao_servico.js
@@ -1,4 +1,4 @@
-function ocultar_servico() {
+function ocultar_servicoo() {
 
     var servico = document.getElementById("servico").value;
     var kg = document.getElementById("kg");
@@ -41,17 +41,21 @@ function ocultar_servico() {
 
   function buscarCepPartida() {
 
+    const enderecoForm = document.querySelector("#enderecoForm");
     const cepInput = document.querySelector("#cep");
     const logradouroInput = document.querySelector("#logradouro");
     const bairroInput = document.querySelector("#bairro");
     const cidadeInput = document.querySelector("#cidade");
     const complementoInput = document.querySelector("#complemento");
     
-    //validação basica do cep
+    //validação basica do cep para ser digitado so numeros
+    //quando precionar a tecla vai ser ativado a função
     cepInput.addEventListener("keypress", (e) => {
      
+     // limitando apenas numero de 0 a 9 digitados
         const onlyNumber = /[0-9]/;
-        //transformando e pegando oque foi digitado para string
+
+        //transformando e pegando oque foi digitado para string para obter o valor exato digitado pelo usuario
         const key = String.fromCharCode(e.keyCode);
     
         //permitir apenas numeros
@@ -62,6 +66,8 @@ function ocultar_servico() {
         }
     });
     
+
+    //pegando os valores quando termina de digitar o 8 digito
     cepInput.addEventListener("keyup" , (e) => {
         //pegando valor do input cep
         const inputValue = e.target.value;
@@ -75,10 +81,27 @@ function ocultar_servico() {
         //obtendo endereço do cliente da API
         const getEndereco = async (cep) =>{
     
+        //removendo o foco do campo cep, 
         cepInput.blur();
+
+        //Url da api do cep, passando para variavel apiUrl
         const apiUrl = `https://viacep.com.br/ws/${cep}/json/`
+
+        //usando a função fetch para uma requisição e await para aguardar a requisição ser concluida e adicionar na variavel response
         const response = await fetch(apiUrl);
-        const data = await response.json();   
+
+        //resposta da requisição convertida em formato JSON e guardado na vaiavel data.
+        const data = await response.json(); 
+       
+
+        if(data.erro){
+         
+          const mensagemErro= document.querySelector("#mensagemErro").innerText="CEP não encontrado. Por favor, verifique o CEP digitado.";
+          limparCampos();
+          return;
+        }else{
+
+          mensagemErro= document.querySelector("#mensagemErro").innerText="";
     
         //mandando a função para habilitar os campos para editar
         toggleDisabled();
@@ -88,7 +111,15 @@ function ocultar_servico() {
         cidadeInput.value = data.localidade
         bairroInput.value = data.bairro
         complementoInput.focus();
+      }
     };
+
+    function limparCampos() {
+      document.getElementById("logradouro").value = "";
+      document.getElementById("bairro").value = "";
+      document.getElementById("cidade").value = "";
+      document.getElementById("estado").value = "";
+  }
     
     //função para habilitar os campos novamente para que o usuario consiga editar
     const toggleDisabled = () => {
@@ -112,9 +143,12 @@ function ocultar_servico() {
     }
 
 
+
+
+
     function buscarCepChegada(){
 
-    const cepInput = document.querySelector("#cep-chegada");
+    const cepInput = document.querySelector("#cep2");
     const logradouroInput = document.querySelector("#logradouro-chegada");
     const bairroInput = document.querySelector("#bairro-chegada");
     const cidadeInput = document.querySelector("#cidade-chegada");
@@ -157,12 +191,18 @@ function ocultar_servico() {
     
         //mandando a função para habilitar os campos para editar
         toggleDisabled();
-    
+        complementoInput.focus();
+       
         //passados valores da api para as variaveis
-        logradouroInput.value = data.logradouro
+        if(data.logradouro==="undefined"){
+          return;
+        }else{
+          logradouroInput.value = data.logradouro
         cidadeInput.value = data.localidade
         bairroInput.value = data.bairro
-        complementoInput.focus();
+        }
+       
+        
     };
     
     //função para habilitar os campos novamente para que o usuario consiga editar
@@ -185,6 +225,3 @@ function ocultar_servico() {
   }
    
 
-
-
-  
