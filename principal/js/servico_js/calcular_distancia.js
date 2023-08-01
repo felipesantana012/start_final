88d86e4d780aa730f4a8b49f1fcbc4230ed49bb0
Felipe Santana <felipe012santana012@gmail.com>
2023-08-01 03:45:01 -0300

    finalizada a pagina de form para enviar dados para o whats do motorista

--- principal/js/servico_js/calcular_distancia.js
+++ principal/js/servico_js/calcular_distancia.js
@@ -1,83 +1,9 @@
 
 
-function calcularDistancia1() {
-    const cep = document.getElementById("cep").value;
-    const cep2 = document.getElementById("cep2").value;
-    const resultadoLabel = document.getElementById("resultado");
-
-    if (!cep || !cep2) {
-        resultadoLabel.innerHTML = `Por favor, digite ambos os CEPs.`;
-        return;
-    }
-
-    const geocoder = new google.maps.Geocoder();
-    const geocoderPromises = [geocodeAddress(geocoder, cep), geocodeAddress(geocoder, cep2)];
-
-    Promise.all(geocoderPromises)
-        .then(results => {
-            const coords1 = results[0].geometry.location;
-            const coords2 = results[1].geometry.location;
-
-
-            const service = new google.maps.DistanceMatrixService();
-            service.getDistanceMatrix(
-                {
-                    origins: [coords1],
-                    destinations: [coords2],
-                    travelMode: google.maps.TravelMode.DRIVING,
-                },
-                (response, status) => {
-                    
-                    if (status === "OK") {
-                        
-                        const distanciaKm = response.rows[0].elements[0].distance.value / 1000; // Distância em quilômetros
-
-                        if(distanciaKm<1){
-                            const distanciaMetros = distanciaKm * 1000;
-                            resultadoLabel.innerHTML = `${distanciaMetros.toFixed(2)} Metros.`;
-                        } else{  
-                        resultadoLabel.innerHTML = `${distanciaKm.toFixed(2)} KM.`;    
-                        }
-                            
-                    } else {
-                         resultadoLabel.innerHTML = `Erro ao calcular a distancia.`;
-                    }
-                }
-            );
-        })
-        .catch(error => {
-            console.log("Ocorreu um erro ao calcular a distância:", error);
-            resultadoLabel.innerHTML = `Cep não encontrado Erro ao calcular a distancia.`;
-        });
-}
-
-function geocodeAddress(geocoder, address) {
-    return new Promise((resolve, reject) => {
-        geocoder.geocode({ address: address }, (results, status) => {
-            if (status === "OK") {
-                resolve(results[0]);
-            } else {
-                reject(status);
-            }
-        });
-    });
-}
-
-// Adicionando evento de escuta (blur event) ao campo de entrada do segundo CEP
-
-
-
-
-
 function calcularDistancia() {
     const cep1 = document.getElementById("cep").value;
     const cep2 = document.getElementById("cep2").value;
     const resultadoInput = document.getElementById("resultado");
-
-    if (!cep1 || !cep2) {
-        alert("Por favor, digite ambos os CEPs.");
-        return;
-    }
 
     const geocoder = new google.maps.Geocoder();
     const geocoderPromises = [geocodeAddress(geocoder, cep1), geocodeAddress(geocoder, cep2)];
@@ -97,7 +23,6 @@ function calcularDistancia() {
                 (response, status) => {
                     if (status === "OK") {
                         const distanciaKm = response.rows[0].elements[0].distance.value / 1000; // Distância em quilômetros
-
                         
                         if (distanciaKm >= 1) {
                             resultadoInput.value = `${distanciaKm.toFixed(2)} KM.`;
@@ -107,14 +32,14 @@ function calcularDistancia() {
                         }
                     } else {
                         console.log("Ocorreu um erro ao calcular a distância:", status);
-                        resultadoInput.value = `Ocorreu um erro ao calcular a distância:`;
+                        resultadoInput.value = `Erro ao calcular a distância, digite os ceps novamente`;
                     }
                 }
             );
         })
         .catch(error => {
             console.log("Ocorreu um erro ao calcular a distância:", error);
-            resultadoInput.value = `Ocorreu um erro ao calcular a distância:`;
+            resultadoInput.value = `Erro ao calcular a distância, digite os ceps novamente`;
         });
 }
 
@@ -129,7 +54,6 @@ function geocodeAddress(geocoder, address) {
         });
     });
 }
-
 // Adicionando evento de escuta (blur event) ao campo de entrada do segundo CEP
 document.getElementById("cep2").addEventListener("blur", function() {
     calcularDistancia();
--- principal/js/servico_js/form_solicitacao_servico.js
+++ principal/js/servico_js/form_solicitacao_servico.js
@@ -41,7 +41,6 @@ function ocultar_servicoo() {
 
   function buscarCepPartida() {
 
-    const enderecoForm = document.querySelector("#enderecoForm");
     const cepInput = document.querySelector("#cep");
     const logradouroInput = document.querySelector("#logradouro");
     const bairroInput = document.querySelector("#bairro");
@@ -76,7 +75,10 @@ function ocultar_servicoo() {
         if(inputValue.length == 8){
             getEndereco(inputValue);
         }
+
     });
+
+       
     
         //obtendo endereço do cliente da API
         const getEndereco = async (cep) =>{
@@ -93,19 +95,18 @@ function ocultar_servicoo() {
         //resposta da requisição convertida em formato JSON e guardado na vaiavel data.
         const data = await response.json(); 
        
-
+        //verificando se a busca do cep obteve algum error
         if(data.erro){
-         
           const mensagemErro= document.querySelector("#mensagemErro").innerText="CEP não encontrado. Por favor, verifique o CEP digitado.";
-          limparCampos();
+        document.getElementById("logradouro").value = "";
+        document.getElementById("bairro").value = "";
+        document.getElementById("cidade").value = "";
           return;
         }else{
-
+          //apagando a mensagem de error da tela
           mensagemErro= document.querySelector("#mensagemErro").innerText="";
-    
         //mandando a função para habilitar os campos para editar
         toggleDisabled();
-    
         //passados valores da api para as variaveis
         logradouroInput.value = data.logradouro
         cidadeInput.value = data.localidade
@@ -114,114 +115,125 @@ function ocultar_servicoo() {
       }
     };
 
-    function limparCampos() {
-      document.getElementById("logradouro").value = "";
-      document.getElementById("bairro").value = "";
-      document.getElementById("cidade").value = "";
-      document.getElementById("estado").value = "";
-  }
+    
     
     //função para habilitar os campos novamente para que o usuario consiga editar
     const toggleDisabled = () => {
     
         //verificando se realmente tem algum imput com a opção desabilitada
         if(logradouroInput.hasAttribute("disabled")){
-          logradouroInput.removeAttribute("disabled");
+            logradouroInput.removeAttribute("disabled");
             complementoInput.removeAttribute("disabled");
             cidadeInput.removeAttribute("disabled");
-            bairroInput.removeAttribute("disabled");
-            
+            bairroInput.removeAttribute("disabled");      
     
      }else {
             //se precisar adicionar desable novamente
-          logradouroInput.setAttribute("desabled","desabled");
+        logradouroInput.setAttribute("desabled","desabled");
         complementoInput.setAttribute("desabled","desabled");
         cidadeInput.setAttribute("desabled","desabled");
-        bairroInput.setAttribute("desabled","desabled");
-                
+        bairroInput.setAttribute("desabled","desabled");           
     }};
     }
 
 
+//--------------------------------------------------------------------------------
 
 
 
     function buscarCepChegada(){
 
-    const cepInput = document.querySelector("#cep2");
-    const logradouroInput = document.querySelector("#logradouro-chegada");
-    const bairroInput = document.querySelector("#bairro-chegada");
-    const cidadeInput = document.querySelector("#cidade-chegada");
-    const complementoInput = document.querySelector("#complemento-chegada");
+    const cep2Input = document.querySelector("#cep2");
+    const logradouro2Input = document.querySelector("#logradouro2");
+    const bairro2Input = document.querySelector("#bairro2");
+    const cidade2Input = document.querySelector("#cidade2");
+    const complemento2Input = document.querySelector("#complemento2");
  
     
-    
-    //validação basica do cep
-    cepInput.addEventListener("keypress", (e) => {
+      //validação basica do cep para ser digitado so numeros
+    //quando precionar a tecla vai ser ativado a função
+    cep2Input.addEventListener("keypress", (e) => {
      
-        const onlyNumber = /[0-9]/;
-        //transformando e pegando oque foi digitado para string
-        const key = String.fromCharCode(e.keyCode);
-    
-        //permitir apenas numeros
-        if(!onlyNumber.test(key)){
-            //so cai nesse if se o usuario digitar algo que não seja numerico
-            e.preventDefault();
-            return;
-        }
-    });
-    
-    cepInput.addEventListener("keyup" , (e) => {
-        //pegando valor do input cep
-        const inputValue = e.target.value;
-    
-        //verificar se tem a quantidade necessaria de digitos
-        if(inputValue.length == 8){
-            getEndereco(inputValue);
-        }
-    });
-    
-        //obtendo endereço do cliente da API
-        const getEndereco = async (cep) =>{
-    
-        cepInput.blur();
-        const apiUrl = `https://viacep.com.br/ws/${cep}/json/`
-        const response = await fetch(apiUrl);
-        const data = await response.json();   
-    
-        //mandando a função para habilitar os campos para editar
-        toggleDisabled();
-        complementoInput.focus();
-       
-        //passados valores da api para as variaveis
-        if(data.logradouro==="undefined"){
-          return;
-        }else{
-          logradouroInput.value = data.logradouro
-        cidadeInput.value = data.localidade
-        bairroInput.value = data.bairro
-        }
-       
+      // limitando apenas numero de 0 a 9 digitados
+         const onlyNumber = /[0-9]/;
+ 
+         //transformando e pegando oque foi digitado para string para obter o valor exato digitado pelo usuario
+         const key = String.fromCharCode(e.keyCode);
+     
+         //permitir apenas numeros
+         if(!onlyNumber.test(key)){
+             //so cai nesse if se o usuario digitar algo que não seja numerico
+             e.preventDefault();
+             return;
+         }
+     });
+     
+ 
+     //pegando os valores quando termina de digitar o 8 digito
+     cep2Input.addEventListener("keyup" , (e) => {
+         //pegando valor do input cep
+         const inputValue = e.target.value;
+     
+         //verificar se tem a quantidade necessaria de digitos
+         if(inputValue.length == 8){
+             getEndereco(inputValue);
+         }
+     });
+     
+         //obtendo endereço do cliente da API
+         const getEndereco = async (cep) =>{
+     
+         //removendo o foco do campo cep, para o usuario não ficar digitando enquanto buscar o cep
+         cep2Input.blur();
+ 
+         //Url da api do cep, passando para variavel apiUrl
+         const apiUrl = `https://viacep.com.br/ws/${cep}/json/`
+ 
+         //usando a função fetch para uma requisição e await para aguardar a requisição ser concluida e adicionar na variavel response
+         const response = await fetch(apiUrl);
+ 
+         //resposta da requisição convertida em formato JSON e guardado na vaiavel data.
+         const data = await response.json(); 
         
-    };
+         //verificando se a busca do cep obteve algum error
+         if(data.erro){
+           const mensagemErro2= document.querySelector("#mensagemErro2").innerText="CEP não encontrado. Por favor, verifique o CEP digitado.";
+        document.getElementById("resultado").value=""
+         document.getElementById("logradouro2").value = "";
+         document.getElementById("bairro2").value = "";
+         document.getElementById("cidade2").value = "";
+           return;
+         }else{
+           //apagando a mensagem de error da tela
+           mensagemErro2= document.querySelector("#mensagemErro2").innerText="";
+         //mandando a função para habilitar os campos para editar
+         toggleDisabled();
+         //passados valores da api para as variaveis
+         logradouro2Input.value = data.logradouro
+         cidade2Input.value = data.localidade
+         bairro2Input.value = data.bairro
+         complemento2Input.focus();
+       }
+     };
+     
+     //função para habilitar os campos novamente para que o usuario consiga editar
+     const toggleDisabled = () => {
+     
+         //verificando se realmente tem algum imput com a opção desabilitada
+         if(logradouro2Input.hasAttribute("disabled")){
+             logradouro2Input.removeAttribute("disabled");
+             complemento2Input.removeAttribute("disabled");
+             cidade2Input.removeAttribute("disabled");
+             bairro2Input.removeAttribute("disabled");      
+     
+      }else {
+             //se precisar adicionar desable novamente
+         logradouro2Input.setAttribute("desabled","desabled");
+         complemento2Input.setAttribute("desabled","desabled");
+         cidade2Input.setAttribute("desabled","desabled");
+         bairro2Input.setAttribute("desabled","desabled");           
+     }};
     
-    //função para habilitar os campos novamente para que o usuario consiga editar
-    const toggleDisabled = () => {
-    
-        //verificando se realmente tem algum imput com a opção desabilitada
-        if(logradouroInput.hasAttribute("disabled")){
-          logradouroInput.removeAttribute("disabled");
-            complementoInput.removeAttribute("disabled");
-            cidadeInput.removeAttribute("disabled");
-            bairroInput.removeAttribute("disabled");       
-    
-     }else {
-            //se precisar adicionar desable novamente
-          logradouroInput.setAttribute("desabled","desabled");
-        complementoInput.setAttribute("desabled","desabled");
-        cidadeInput.setAttribute("desabled","desabled");
-        bairroInput.setAttribute("desabled","desabled");         
-    }};
   }
    
 
